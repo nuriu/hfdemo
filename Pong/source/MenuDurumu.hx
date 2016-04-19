0bd5360b17db38aaff23682c35e7f97cc9837bab
nuriu <nuriu@users.noreply.github.com>
2016-04-19 11:34:35 +0300

    ikinci oyuncu için paddle eklendi. paddle renklendirmesi yapıldı.

--- Pong/source/MenuDurumu.hx
+++ Pong/source/MenuDurumu.hx
@@ -6,41 +6,56 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.util.FlxColor;
 
 class MenuDurumu extends FlxState
 {
-	var oyuncu:Paddle;
+	private var oyuncu:Paddle;
+	private var oyuncu2:Paddle;
+
 	override public function create():Void
 	{
 		super.create();
 
-		// birinci paddle
-		oyuncu = new Paddle(10, 10);
+		// birinci oyuncu
+		oyuncu = new Paddle(10, 10, FlxColor.WHITE);
 		add(oyuncu);
+
+		// ikinci oyuncu
+		oyuncu2 = new Paddle(620, 10, FlxColor.RED);
+		add(oyuncu2);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 
-		// birinci paddle hareketleri
-		if (FlxG.keys.pressed.UP)
-		{
+		// birinci oyuncu hareketleri
+		if (FlxG.keys.pressed.W)
 			oyuncu.y -= 5;
-		}
-		if (FlxG.keys.pressed.DOWN)
-		{
-			oyuncu.y += 5;
-		}
 
-		// ekrandan dışarı çıkmasını önleme
+		if (FlxG.keys.pressed.S)
+			oyuncu.y += 5;
+
+		// birinci oyuncunun ekrandan dışarı çıkmasını önleme
 		if (oyuncu.y < 0)
-		{
 			oyuncu.y = 0;
-		}
+
 		if (oyuncu.y > FlxG.stage.stageHeight - oyuncu.height)
-		{
 			oyuncu.y = FlxG.stage.stageHeight - oyuncu.height;
-		}
+
+		// ikinci oyuncu hareketleri
+		if (FlxG.keys.pressed.UP)
+			oyuncu2.y -= 5;
+
+		if (FlxG.keys.pressed.DOWN)
+			oyuncu2.y += 5;
+
+		// ikinci oyuncunun ekrandan dışarı çıkmasını önleme
+		if (oyuncu2.y < 0)
+			oyuncu2.y = 0;
+		
+		if (oyuncu2.y > FlxG.stage.stageHeight - oyuncu2.height)
+			oyuncu2.y = FlxG.stage.stageHeight - oyuncu2.height;
 	}
 }
--- Pong/source/Paddle.hx
+++ Pong/source/Paddle.hx
@@ -2,13 +2,14 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.util.FlxColor;
 
 class Paddle extends FlxSprite
 {
-    public function new(x:Float, y:Float)
+    public function new(x:Float, y:Float, renk:FlxColor)
     {
         super(x, y);
-        makeGraphic(10, 100, 0xFFFFFFFF);
+        makeGraphic(10, 100, renk);
     }
 
     override public function update(elapsed:Float):Void
